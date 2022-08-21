a647af8de749e8f81c84b0fcd45c030a233b3872
Neesham <neeshamraghav0@gmail.com>
2022-08-21 16:03:53 +0530

    Made it better

--- myapp/settings.py
+++ myapp/settings.py
@@ -97,12 +97,16 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+"""
 CORS_ALLOWED_ORIGINS = [
 
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
     "https://cosb.live",
 ]
+"""
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_METHODS = [
 
